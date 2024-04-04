27b6950617d599197809b3ca2c175a26b87a5a01
magapeio <dev@magape.io>
2024-04-04 16:12:27 +0800

    Remove burning

--- GameAsset/NFTFee.sol
+++ GameAsset/NFTFee.sol
@@ -6,24 +6,17 @@ import {Hashes} from "../Util/Hashes.sol";
 contract NFTFee is Hashes {
     function _pay(uint256 amt) internal {
         assembly {
-            let tkn := sload(TTF)
-            
-            // require(ERC20(TFM).transferFrom(msg.sender, OWO, amt));
+            // require(ERC20(TFM).transferFrom(msg.sender, address(this), amt));
             mstore(0x80, TFM)
             mstore(0x84, caller())
             mstore(0xa4, address())
             mstore(0xc4, amt)
-            if iszero(call(gas(), tkn, 0x00, 0x80, 0x64, 0x00, 0x00)) {
+            if iszero(call(gas(), sload(TTF), 0x00, 0x80, 0x64, 0x00, 0x00)) {
                 mstore(0x80, ERR)
                 mstore(0xa0, STR)
                 mstore(0xc0, ER5)
                 revert(0x80, 0x64)
             }
-
-            // ERC20(TTF).burn(amt)
-            mstore(0x80, 0x42966c6800000000000000000000000000000000000000000000000000000000)
-            mstore(0x84, amt)
-            pop(call(gas(), tkn, 0x00, 0x80, 0x24, 0x00, 0x00))
         }
     }
 }
