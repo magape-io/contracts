d8f9641f2c9b6a011d147489e3aef93b84480ed9
magapeio <dev@magape.io>
2024-05-24 11:14:21 +0800

    Renamed

--- Deploy/Deploy_Arena.sol
+++ Deploy/Deploy_Arena.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.0;
 import {Upgrade} from "../Deploy/Upgrade.sol";
 import {Distribute} from "../Market/Distribute.sol";
 
-contract Insert is Upgrade(address(0x00)) {
+contract Arena is Upgrade(address(0x00)) {
     constructor() {
         address dis = address(new Distribute());
         assembly {
