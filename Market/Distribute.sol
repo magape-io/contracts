2d08f4d63d814ffbc559148a0c1b4239b630ab47
magapeio <dev@magape.io>
2024-05-24 17:42:04 +0800

    Revert back to Yul, added checks for transfer, err: event not shown, maybe due to low gas

--- Market/Distribute.sol
+++ Market/Distribute.sol
@@ -3,66 +3,48 @@ pragma solidity 0.8.0;
 
 import {Hashes} from "../Util/Hashes.sol";
 
-interface IERC20 {
-    function transferFrom(
-        address,
-        address,
-        uint256
-    ) external;
-
-    function transfer(address, uint256) external;
-}
-
 contract Distribute is Hashes {
     function distribute(uint256 amt, address tkn) external {
-        address owner;
-        address recipient;
         assembly {
-            owner := sload(OWO)
-            recipient := sload(tkn)
+            // require(ERC20(TFM).transferFrom(msg.sender, address(this), amt));
+            mstore(0x80, TFM)
+            mstore(0x84, caller())
+            mstore(0xa4, address())
+            mstore(0xc4, amt)
+            if iszero(call(gas(), tkn, 0x00, 0x80, 0x64, 0x00, 0x00)) {
+                mstore(0x80, ERR)
+                mstore(0xa0, STR)
+                mstore(0xc0, ER5)
+                revert(0x80, 0x64)
+            }
+
+            let fee := div(amt, sload(TFM))
+
+            // ERC20(tkn).transfer(_owner, fee);
+            mstore(0x80, TTF)
+            mstore(0x84, sload(OWO))
+            mstore(0xa4, fee)
+            // pop(call(gas(), tkn, 0x00, 0x80, 0x44, 0x00, 0x00))
+            if iszero(call(gas(), tkn, 0x00, 0x80, 0x44, 0x00, 0x00)) {
+                mstore(0x80, ERR)
+                mstore(0xa0, STR)
+                mstore(0xc0, ER5)
+                revert(0x80, 0x64)
+            }
+
+            // ERC20(tkn).transfer(pay, amt - fee);
+            // Every new game will have a token that is mapped to
+            // a receiving address. This address can be set from
+            // Upgrade.sol mem(token_address, receiver_address)
+            mstore(0x84, sload(tkn))
+            mstore(0xa4, sub(amt, fee))
+            // pop(call(gas(), tkn, 0x00, 0x80, 0x44, 0x00, 0x00))
+            if iszero(call(gas(), tkn, 0x00, 0x80, 0x44, 0x00, 0x00)) {
+                mstore(0x80, ERR)
+                mstore(0xa0, STR)
+                mstore(0xc0, ER5)
+                revert(0x80, 0x64)
+            }
         }
-        IERC20 ierc20 = IERC20(tkn);
-        ierc20.transferFrom(msg.sender, address(this), amt);
-        ierc20.transfer(owner, amt / 5);
-        ierc20.transfer(recipient, (amt * 4) / 5);
-        // assembly {
-        //     // require(ERC20(TFM).transferFrom(msg.sender, address(this), amt));
-        //     mstore(0x80, TFM)
-        //     mstore(0x84, caller())
-        //     mstore(0xa4, address())
-        //     mstore(0xc4, amt)
-        //     if iszero(call(gas(), tkn, 0x00, 0x80, 0x64, 0x00, 0x00)) {
-        //         mstore(0x80, ERR)
-        //         mstore(0xa0, STR)
-        //         mstore(0xc0, ER5)
-        //         revert(0x80, 0x64)
-        //     }
-
-        //     let fee := div(amt, sload(TFM))
-
-        //     // ERC20(tkn).transfer(_owner, fee);
-        //     mstore(0x80, TTF)
-        //     mstore(0x84, sload(OWO))
-        //     mstore(0xa4, fee)
-        //     if iszero(call(gas(), tkn, 0x00, 0x80, 0x44, 0x00, 0x00)) {
-        //         mstore(0x80, ERR)
-        //         mstore(0xa0, STR)
-        //         mstore(0xc0, ER5)
-        //         revert(0x80, 0x64)
-        //     }
-
-        //     // ERC20(tkn).transfer(pay, amt - fee);
-        //     // Every new game will have a token that is mapped to
-        //     // a receiving address. This address can be set from
-        //     // Upgrade.sol mem(token_address, receiver_address)
-        //     mstore(0x84, sload(tkn))
-        //     mstore(0xa4, sub(amt, fee))
-        //     if iszero(call(gas(), tkn, 0x00, 0x80, 0x44, 0x00, 0x00)) {
-        //         mstore(0x80, ERR)
-        //         mstore(0xa0, STR)
-        //         mstore(0xc0, ER5)
-        //         revert(0x80, 0x64)
-        //     }
-        // }
     }
 }
