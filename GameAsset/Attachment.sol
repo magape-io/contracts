dffd4c2a7d26578812235c8c6820df0565ad5190
magapeio <dev@magape.io>
2024-05-24 17:42:14 +0800

    Added check for transfer

--- GameAsset/Attachment.sol
+++ GameAsset/Attachment.sol
@@ -31,7 +31,12 @@ contract Attachment is ECDSA, NFTFee, Top5, Ownable {
             mstore(0x80, TTF)
             mstore(0x84, caller())
             mstore(0xa4, amt)
-            pop(call(gas(), sload(TTF), 0x00, 0x80, 0x44, 0x00, 0x00))
+            if iszero(call(gas(), sload(TTF), 0x00, 0x80, 0x44, 0x00, 0x00)) {
+                mstore(0x80, ERR)
+                mstore(0xa0, STR)
+                mstore(0xc0, ER5)
+                revert(0x80, 0x64)
+            }
         }
     }
 
