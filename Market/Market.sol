16727c3f70d507d91e6fa5a1aa5e04136a3104ac
magapeio <dev@magape.io>
2024-06-21 13:04:08 +0800

    Renamed

--- Market/Market.sol
+++ Market/Market.sol
@@ -1,111 +1,106 @@
 // SPDX-License-Identifier: None
 pragma solidity 0.8.0;
 
-import {DynamicPrice} from "../Util/DynamicPrice.sol";
-import {Merch} from "../Market/Merch.sol";
-import {P2P} from "../Market/P2P.sol";
+import {Hashes} from "../Util/Hashes.sol";
 
-contract Market is Merch, DynamicPrice, P2P {
-    event List(address indexed, uint256 indexed, address indexed, uint256);
-    event Buy(address indexed, address indexed, uint256 indexed);
-    bytes32 private constant ELI =
-        0x9b2c2060125acbb354a4a920d52588cb0024b0eead356cfc9f62c3b041fe3c63;
-    bytes32 private constant EBU =
-        0xd0c183be209f70036b50de16805d88249019e1288d7b77ef877710999c0d08e6;
+contract Market is Hashes {
+    // game, receiver, sender, bid, amt
+    event Buy(
+        address indexed,
+        address indexed,
+        address indexed,
+        uint256,
+        uint256
+    );
 
-    function list(
-        address adr,
-        uint256 tid,
-        address tkn,
-        uint256 amt
+    // bulk purchase
+    function buy(
+        address gam,
+        uint256 ttl,
+        address[] calldata rcv,
+        uint256[] calldata bid,
+        uint256[] calldata amt
     ) external {
-        // tkn - 0: 下架，1：货币，>1: 代币
         assembly {
-            mstore(0x00, amt) // emit List
-            log4(0x00, 0x20, ELI, adr, tid, tkn)
-
-            mstore(0x80, OWO) // ownerOf(id)
-            mstore(0x84, tid)
-            pop(staticcall(gas(), adr, 0x80, 0x24, 0x00, 0x20))
-
-            if iszero(eq(mload(0x0), caller())) {
-                // require(ownerOf(tokenId) == msg.sender, 0xf)
-                mstore(0x80, ERR)
-                mstore(0xa0, STR)
-                mstore(0xc0, ER2)
-                revert(0x80, 0x64)
-            }
-
-            if gt(tkn, 0x0) {
-                // if(tokenAddr > 0)
-                mstore(0x80, AFA) // isApprovedForAll(address, address)
-                mstore(0x84, caller())
-                mstore(0xa4, address())
-                pop(staticcall(gas(), adr, 0x80, 0x44, 0x00, 0x20))
-
-                if iszero(mload(0x00)) {
-                    // require(isApprovedForAll(msg.sender, address(this)))
-                    mstore(0x80, ERR)
-                    mstore(0xa0, STR)
-                    mstore(0xc0, ER2)
-                    revert(0x80, 0x64)
-                }
-            }
-
-            mstore(0x00, adr) // listData[adr][tid] = (tkn, amt)
-            mstore(0x20, tid)
-            let tmp := keccak256(0x00, 0x40)
-            sstore(tmp, tkn)
-            sstore(add(tmp, 0x01), amt)
-        }
-    }
-
-    // 单买
-    function buy(address adr, uint256 tid) public payable {
-        bytes32 tmp;
-
-        assembly {
-            log4(0x00, 0x00, EBU, caller(), adr, tid)
-
-            mstore(0x80, OWO) // Item(adr).ownerOf(tid)
-            mstore(0x84, tid)
-            pop(staticcall(gas(), adr, 0x80, 0x24, 0x00, 0x20))
-            let oid := mload(0x0)
-
-            mstore(0x00, adr) // listData[adr][tid]
-            mstore(0x20, tid)
-            tmp := keccak256(0x00, 0x40) // require(listData[adr][tid].address != address(0))
-            if iszero(sload(tmp)) {
+            // (bool isActive, uint256 amtPool) = Node(adr).games(mer);
+            mstore(
+                0x80,
+                0x79131a1900000000000000000000000000000000000000000000000000000000
+            )
+            mstore(0x84, gam)
+            pop(staticcall(gas(), sload(ER5), 0x80, 0x84, 0x00, 0x40))
+            // require(isActive || amtPool >= MAC);
+            if and(iszero(mload(0x00)), lt(mload(0x20), sload(ER5))) {
                 mstore(0x80, ERR)
                 mstore(0xa0, STR)
                 mstore(0xc0, ER1)
                 revert(0x80, 0x64)
             }
 
-            mstore(0x80, TFM) // Item(adr).transferFrom(oid, msg.sender, tid)
-            mstore(0x84, oid)
-            mstore(0xa4, caller())
-            mstore(0xc4, tid)
-            if iszero(call(gas(), adr, 0x00, 0x80, 0x64, 0x00, 0x00)) {
+            // require(MAC(TTF).transferForm(msg.sender, address(this), ttl))
+            mstore(0x80, TFM)
+            mstore(0x84, caller())
+            mstore(0xa4, address())
+            mstore(0xc4, ttl)
+            if iszero(call(gas(), sload(TTF), 0x00, 0x80, 0x64, 0x00, 0x00)) {
                 mstore(0x80, ERR)
                 mstore(0xa0, STR)
                 mstore(0xc0, ER2)
                 revert(0x80, 0x64)
             }
-            adr := oid
+
+            // reset ttl to transfer to admin
+            ttl := 0x00
         }
-
-        _pay(tmp, adr, 0x01); // 转币给卖家减费用
-
-        assembly {
-            sstore(tmp, 0x00) // 下架 listData[adr][tid] = new List(address(0), 0)
-        }
-    }
-
-    // 量买
-    function buy(address[] memory adr, uint256[] memory tid) external payable {
         unchecked {
-            for (uint256 i; i < adr.length; ++i) buy(adr[i], tid[i]);
+            for (uint256 i; i < rcv.length; ++i) {
+                (address rc1, uint256 bi1, uint256 am1) = (
+                    rcv[i],
+                    bid[i],
+                    amt[i]
+                );
+                assembly {
+                    // ttl += ttl*0.05
+                    ttl := add(ttl, div(am1, 0x14)) // admin 5%
+
+                    // require(transfer(rc1, am1));
+                    mstore(0x80, TTF)
+                    mstore(0x84, rc1)
+                    mstore(0xa4, div(mul(am1, 0x5f), 0x64)) // receiver 95% (wrong)
+                    if iszero(
+                        call(gas(), sload(TTF), 0x00, 0x80, 0x44, 0x00, 0x00)
+                    ) {
+                        mstore(0x80, ERR)
+                        mstore(0xa0, STR)
+                        mstore(0xc0, ER5)
+                        revert(0x80, 0x64)
+                    }
+
+                    // emit Buy(gam, rc1, msg.sender, bi1, am1)
+                    mstore(0x00, bi1)
+                    mstore(0x20, am1)
+                    log4(
+                        0x00,
+                        0x40,
+                        0xbab4aa6b2d5c0935e0e2937d1f73655848f670d43bf6f0c7e9e11e635bb5d86f,
+                        gam,
+                        caller(),
+                        rc1
+                    )
+                }
+            }
+        }
+        assembly {
+            // require(transfer(OWO, ttl));
+            mstore(0x80, TTF)
+            mstore(0x84, sload(OWO))
+            mstore(0xa4, ttl)
+            if iszero(call(gas(), sload(TTF), 0x00, 0x80, 0x44, 0x00, 0x00)) {
+                mstore(0x80, ERR)
+                mstore(0xa0, STR)
+                mstore(0xc0, ER5)
+                revert(0x80, 0x64)
+            }
         }
     }
 }
