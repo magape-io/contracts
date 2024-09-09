abec6e634f14c433360ca56d6d04c9fdc8f1274f
magapeio <dev@magape.io>
2024-09-09 17:04:54 +0800

    combined resourceIn

--- Governance/Resources.sol
+++ Governance/Resources.sol
@@ -23,7 +23,7 @@ contract Resources is Hashes, ECDSA {
         }
     }
 
-    function resourceIn(address adr, uint256 amt) external {
+    function resourceIn(uint256 bid, address adr, uint256 amt) external {
         assembly {
             // game[adr].amt += amt;
             let tmp := shl(0x06, adr)
@@ -32,12 +32,7 @@ contract Resources is Hashes, ECDSA {
             // emit Transfer(adr, ad2, amt)
             mstore(0x00, amt)
             log3(0x00, 0x20, ETF, caller(), adr)
-        }
-        _transferFrom(amt);
-    }
 
-    function resourceIn(uint256 bid, uint256 amt) external {
-        assembly {
             // emit P2PBuy(bid, amt)
             mstore(0x00, bid)
             mstore(0x20, amt)
@@ -47,8 +42,19 @@ contract Resources is Hashes, ECDSA {
                 0x5b6b68c7bdbd1a2670386a943b40bb818dd5806b1cbfdbe38b4b399a8a7e6eab,
                 caller()
             )
+
+            // require(MAC(TTF).transferForm(msg.sender, address(this), amt))
+            mstore(0x80, TFM)
+            mstore(0x84, caller())
+            mstore(0xa4, address())
+            mstore(0xc4, amt)
+            if iszero(call(gas(), sload(TTF), 0x00, 0x80, 0x64, 0x00, 0x00)) {
+                mstore(0x80, ERR)
+                mstore(0xa0, STR)
+                mstore(0xc0, ER2)
+                revert(0x80, 0x64)
+            }
         }
-        _transferFrom(amt);
     }
 
     function resourceOut(
