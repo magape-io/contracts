d9e4e6293bb625e5f6d9f5b842ae756c9e5621bd
magapeio <dev@magape.io>
2024-09-12 11:49:08 +0800

    Remove unsued event P2PBuy

--- Governance/Resources.sol
+++ Governance/Resources.sol
@@ -5,7 +5,6 @@ import {Hashes} from "../Util/Hashes.sol";
 import {ECDSA} from "../Util/ECDSA.sol";
 
 contract Resources is Hashes, ECDSA {
-    event P2PBuy(uint256, uint256);
 
     function _transferFrom(uint256 amt) private {
         assembly {
@@ -23,52 +22,17 @@ contract Resources is Hashes, ECDSA {
         }
     }
 
-    function resourceIn(address adr, uint256 bid, uint256 amt) external {
+    function resourceIn(address adr, uint256, uint256 amt) external {
         assembly {
             // game[adr].amt += amt;
             let tmp := shl(0x06, adr)
             sstore(tmp, add(sload(tmp), amt))
 
-            // emit P2PBuy(bid, amt)
-            mstore(0x00, bid)
-            mstore(0x20, amt)
-            log2(
-                0x00,
-                0x40,
-                0x5b6b68c7bdbd1a2670386a943b40bb818dd5806b1cbfdbe38b4b399a8a7e6eab,
-                caller()
-            )
 
             // require(MAC(TTF).transferForm(msg.sender, address(this), amt))
             mstore(0x80, TFM)
             mstore(0x84, caller())
             mstore(0xa4, address())
-            mstore(0xc4, amt)
-            if iszero(call(gas(), sload(TTF), 0x00, 0x80, 0x64, 0x00, 0x00)) {
-                mstore(0x80, ERR)
-                mstore(0xa0, STR)
-                mstore(0xc0, ER2)
-                revert(0x80, 0x64)
-            }
-        }
-    }
-
-    function resourceIn(uint256 bid, address adr, uint256 amt) external {
-        assembly {
-            // emit P2PBuy(bid, amt)
-            mstore(0x00, bid)
-            mstore(0x20, amt)
-            log2(
-                0x00,
-                0x40,
-                0x5b6b68c7bdbd1a2670386a943b40bb818dd5806b1cbfdbe38b4b399a8a7e6eab,
-                caller()
-            )
-
-            // require(MAC(TTF).transferForm(msg.sender, address(this), amt))
-            mstore(0x80, TFM)
-            mstore(0x84, caller())
-            mstore(0xa4, adr)
             mstore(0xc4, amt)
             if iszero(call(gas(), sload(TTF), 0x00, 0x80, 0x64, 0x00, 0x00)) {
                 mstore(0x80, ERR)
